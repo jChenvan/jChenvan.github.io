11c0bd2041f7d22c54779d1d51e03fbcfac2bf95
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-06-17 10:58:49 -0400

    Add preview videos, Design overhaul

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,6 +1,6 @@
 export default function Contacts() {
-    return <section className='flex flex-col box-glow rounded-lg p-4 m-6 items-center'>
-          <h2 className='text-4xl text-center'>Contacts</h2>
+    return <section className='flex flex-col shadow-lg shadow-black bg-green-700 text-white rounded-lg p-4 m-6 items-center' id="contacts">
+          <h2 className='text-4xl text-center self-stretch pb-4 border-b-2'>Contacts</h2>
           <div className='flex gap-10 my-4'>
             <a href="mailto:jchenvan@uwaterloo.ca" className='hover:opacity-70 transition-all'>
               <img src="/email.svg" alt="" className='h-[100px]'/>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,16 +1,21 @@
-import skills from "../site data/skills.json"
+import React from "react"
+import skills from "../site data/skills"
+
+
 
 export default function Skills () {
-    return <section className='flex flex-col items-center box-glow rounded-lg p-4 m-6'>
-          <h2 className='text-4xl'>My Skills</h2>
+    return <section className='flex flex-col items-center shadow-lg shadow-black rounded-lg p-4 m-6 bg-red-700 text-white' id="skills">
+          <h2 className='text-4xl self-stretch border-b-2 text-center pb-4'>My Skills</h2>
           <p className='text-center my-4'>Through my time in University, as well as online coding resources like The <a href="https://www.theodinproject.com/">Odin Project</a>, I have become proficient in the many facets of web development. </p>
-          <div className='flex flex-wrap gap-6'>
-            {Object.entries(skills).map(([area, skills]) => <div key={area}>
-              <h3 className='mb-2 w-[10ch]'>{area}</h3>
-              <ul>
-                {skills.map((skill,index) => <li key={index} className="before:content-['>'] before:mr-1">{skill}</li>)}
-              </ul>
-            </div>)}
+          <div style={{gridTemplateColumns: 'auto auto'}} className='grid gap-y-2 gap-x-3 bg-red-950 p-2 rounded-md'>
+            {Object.entries(skills).map(([area,skills],index)=><React.Fragment key={index}>
+              <div>
+                {area}
+              </div>
+              <div>
+                {skills.join(', ')}
+              </div>
+            </React.Fragment>)}
           </div>
         </section>
 }
