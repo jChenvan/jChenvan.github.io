c291806a67ea2afd7b2c98166a4c870eeea37e57
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-06-18 15:36:05 -0400

    Revert "Site now fetches skills/projects from external source"
    This reverts commit 3361d7f541cdc7a55a10fc962279f9affe5aec92.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,21 +1,21 @@
 import React from "react"
-import useJson from "../hooks/useJson"
+import skills from "../site data/skills"
+
+
 
 export default function Skills () {
-  const {json:skills} = useJson("skills.json");
-
-  return <section className='flex flex-col items-center shadow-lg shadow-black rounded-lg p-4 m-6 bg-red-700 text-white' id="skills">
-        <h2 className='text-4xl self-stretch border-b-2 text-center pb-4'>My Skills</h2>
-        <p className='text-center my-4'>Through my time in University, as well as online coding resources like The <a href="https://www.theodinproject.com/">Odin Project</a>, I have become proficient in the many facets of web development. </p>
-        <div style={{gridTemplateColumns: 'auto auto'}} className='grid gap-y-2 gap-x-3 bg-red-950 p-2 rounded-md'>
-          {skills && Object.entries(skills).map(([area,skills],index)=><React.Fragment key={index}>
-            <div>
-              {area}
-            </div>
-            <div>
-              {(skills as string[]).join(', ')}
-            </div>
-          </React.Fragment>)}
-        </div>
-      </section>
+    return <section className='flex flex-col items-center shadow-lg shadow-black rounded-lg p-4 m-6 bg-red-700 text-white' id="skills">
+          <h2 className='text-4xl self-stretch border-b-2 text-center pb-4'>My Skills</h2>
+          <p className='text-center my-4'>Through my time in University, as well as online coding resources like The <a href="https://www.theodinproject.com/">Odin Project</a>, I have become proficient in the many facets of web development. </p>
+          <div style={{gridTemplateColumns: 'auto auto'}} className='grid gap-y-2 gap-x-3 bg-red-950 p-2 rounded-md'>
+            {Object.entries(skills).map(([area,skills],index)=><React.Fragment key={index}>
+              <div>
+                {area}
+              </div>
+              <div>
+                {skills.join(', ')}
+              </div>
+            </React.Fragment>)}
+          </div>
+        </section>
 }
---
+++ src/site data/skills.ts
@@ -0,0 +1,38 @@
+const skills = {
+    "Programming Languages": [
+        "Javascript",
+        "Python",
+        "C",
+        "R"
+    ],
+    "Backend": [
+        "NodeJs",
+        "ExpressJs",
+        "REST",
+        "Prisma ORM",
+        "Firebase"
+    ],
+    "Frontend": [
+        "HTML",
+        "CSS",
+        "React",
+        "TailwindCSS"
+    ],
+    "Mobile": [
+        "Flutter"
+    ],
+    "Database": [
+        "PostgreSQL",
+        "MySQL"
+    ],
+    "Operating Systems": [
+        "Linux",
+        "Windows",
+        "MacOS"
+    ],
+    "Other": [
+        "Blender"
+    ]
+}
+
+export default skills;
