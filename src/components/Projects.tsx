856e185d40186fb26adae80af832d80381c5108c
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-06-18 15:36:05 -0400

    Revert "Site now fetches skills/projects from external source"
    This reverts commit ed70ac7dc35e0c7b5217a18222cbdea0c682cd52.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,21 +1,16 @@
 import { useEffect, useRef, useState } from "react";
-import useJson from "../hooks/useJson";
+import projects from "../site data/projects"
 
 export default function Projects() {
-    const {json:projects} = useJson('projects.json');
-    const [currentProject, setCurrentProject] = useState<string>();
+    const [currentProject, setCurrentProject] = useState(Object.keys(projects)[0]);
     const videoRef = useRef<HTMLVideoElement>(null);
-
-    useEffect(()=>{
-      if (projects) setCurrentProject(Object.keys(projects)[0]);
-    },[projects]);
 
     useEffect(()=>{
       const vid = videoRef.current;
       if (vid) vid.load();
     }, [currentProject]);
 
-    return projects && currentProject && projects[currentProject] ? <section className='shadow-lg shadow-black rounded-lg p-4 m-6 bg-blue-800 text-white' id="projects">
+    return <section className='shadow-lg shadow-black rounded-lg p-4 m-6 bg-blue-800 text-white' id="projects">
           <div className='relative'>
             <h2 className='text-4xl text-center pb-4 border-b-2'>My Projects</h2>
             <select id="project" value={currentProject} onChange={e=>setCurrentProject(e.target.value)} className='absolute top-0 right-0 bg-black p-2 rounded-md'>
@@ -36,9 +31,9 @@ export default function Projects() {
             </div>
             <div>
               <ul className="w-fit my-4 mx-auto">
-                {Object.entries(projects[currentProject as keyof typeof projects].links).map(([text, url])=><li key={url as string} className='bg-blue-950 text-center mt-2 p-2 rounded-md rounded-br-none hover:bg-blue-500 transition-all'><a href={url as string}>{text}</a></li>)}
+                {Object.entries(projects[currentProject as keyof typeof projects].links).map(([text, url])=><li key={url as string} className='bg-blue-950 text-center mt-2 p-2 rounded-md rounded-br-none hover:bg-blue-500 transition-all'><a href={url}>{text}</a></li>)}
               </ul>
             </div>
           </div>
-        </section> : <></>;
+        </section>;
 }
